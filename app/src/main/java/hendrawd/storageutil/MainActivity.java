82ebd8a4e9cb4a0d58d54be6740e84ccccc6aea2
HendraWD <hendraz_88@yahoo.co.id>
2019-10-24 02:58:27 +0700

    Update dependencies

--- app/src/main/java/hendrawd/storageutil/MainActivity.java
+++ app/src/main/java/hendrawd/storageutil/MainActivity.java
@@ -1,7 +1,7 @@
 package hendrawd.storageutil;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spanned;
 import android.util.TypedValue;
